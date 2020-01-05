43f75c82ed82e7e2971088f43ec1bc409a4032bd
Keith Winstein <keithw@cs.stanford.edu>
2020-01-04 17:42:38 -0800

    fun.cc: further simplify (no need to wait explicitly for TCP connect)

--- src/examples/fun.cc
+++ src/examples/fun.cc
@@ -36,59 +36,38 @@ void program_body()
   SSLContext ssl_context;
   SSLSession ssl { ssl_context.make_SSL_handle(), move( tcp_sock ) };
 
-  {
-    bool connected = false;
-    EventLoop event_loop;
-    event_loop.add_rule(
-      ssl.socket(),
-      Direction::Out,
-      [&] {
-        ssl.socket().throw_if_error();
-        ssl.socket().peer_address();
-        connected = true;
-      },
-      [&] { return not connected; },
-      [] {},
-      [&] { ssl.socket().throw_if_error(); } );
-
-    while ( event_loop.wait_next_event( -1 ) != EventLoop::Result::Exit ) {
-    }
-  }
-
   const string query = "GET /~keithw/ HTTP/1.1\r\nHost: cs.stanford.edu\r\n\r\nGET /~keithw/test.txt HTTP/1.1\r\nHost: "
                        "cs.stanford.edu\r\nConnection: close\r\n\r\n";
 
   if ( not ssl.outbound_plaintext().read_from( query ).empty() ) {
-    throw runtime_error( "no room to send query" );
+    throw runtime_error( "no room to write query" );
   }
 
-  {
-    EventLoop event_loop;
+  EventLoop event_loop;
 
-    event_loop.add_rule(
-      ssl.socket(),
-      Direction::In,
-      [&] { ssl.do_read(); },
-      [&] { return ssl.want_read(); },
-      [] {},
-      [&] { ssl.socket().throw_if_error(); } );
+  event_loop.add_rule(
+    ssl.socket(),
+    Direction::In,
+    [&] { ssl.do_read(); },
+    [&] { return ssl.want_read(); },
+    [] {},
+    [&] { ssl.socket().throw_if_error(); } );
 
-    event_loop.add_rule(
-      ssl.socket(),
-      Direction::Out,
-      [&] { ssl.do_write(); },
-      [&] { return ssl.want_write(); },
-      [] {},
-      [&] { ssl.socket().throw_if_error(); } );
+  event_loop.add_rule(
+    ssl.socket(),
+    Direction::Out,
+    [&] { ssl.do_write(); },
+    [&] { return ssl.want_write(); },
+    [] {},
+    [&] { ssl.socket().throw_if_error(); } );
 
-    event_loop.add_rule(
-      standard_output,
-      Direction::Out,
-      [&] { ssl.inbound_plaintext().write_to( standard_output ); },
-      [&] { return !ssl.inbound_plaintext().readable_region().empty(); } );
+  event_loop.add_rule(
+    standard_output,
+    Direction::Out,
+    [&] { ssl.inbound_plaintext().write_to( standard_output ); },
+    [&] { return !ssl.inbound_plaintext().readable_region().empty(); } );
 
-    while ( event_loop.wait_next_event( -1 ) != EventLoop::Result::Exit ) {
-    }
+  while ( event_loop.wait_next_event( -1 ) != EventLoop::Result::Exit ) {
   }
 }
 
