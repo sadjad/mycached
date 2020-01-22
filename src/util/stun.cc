1e91924c643910e908a45670c602c2280f117df0
Keith Winstein <keithw@cs.stanford.edu>
2020-01-22 00:44:07 -0800

    Implement STUN locally, since libnice does not ship a static lib on Debian

--- src/util/stun.cc
+++ src/util/stun.cc
@@ -1,59 +1,80 @@
 #include "stun.hh"
+#include "random.hh"
+#include "timer.hh"
 
-extern "C"
-{
-#include <stun/usages/bind.h>
-}
+#include <iomanip>
+#include <iostream>
 
 using namespace std;
 
-STUNClient::STUNClient()
-  : agent_()
+void STUNClient::expire_pending_requests()
 {
-  stun_agent_init(
-    &agent_, STUN_ALL_KNOWN_ATTRIBUTES, STUN_COMPATIBILITY_RFC3489, STUN_AGENT_USAGE_IGNORE_CREDENTIALS );
+  const uint64_t now = timestamp_ns();
+
+  for ( auto it = pending_requests_.begin(); it != pending_requests_.end(); ) {
+    if ( it->second < now ) {
+      it = pending_requests_.erase( it );
+    } else {
+      ++it;
+    }
+  }
 }
 
-string_view STUNClient::make_binding_request()
+string STUNClient::make_binding_request()
 {
-  StunMessage message;
+  expire_pending_requests();
 
-  scratch_.resize( 64 );
+  string transaction_id;
 
-  const size_t len
-    = stun_usage_bind_create( &agent_, &message, reinterpret_cast<uint8_t*>( scratch_.data() ), scratch_.size() );
+  for ( unsigned int i = 0; i < 16; i++ ) {
+    transaction_id.push_back( dist_( rng_ ) );
+  }
 
-  return { scratch_.data(), len };
+  pending_requests_.emplace( transaction_id, timestamp_ns() + uint64_t( 5 ) * 1000 * 1000 * 1000 );
+
+  return string( "\x00\x01\x00\x00", 4 ) + transaction_id;
 }
 
 optional<Address> STUNClient::process_binding_response( const string_view buffer )
 {
-  StunMessage message;
-  if ( STUN_VALIDATION_SUCCESS
-       != stun_agent_validate(
-         &agent_, &message, reinterpret_cast<const uint8_t*>( buffer.data() ), buffer.size(), nullptr, nullptr ) ) {
+  expire_pending_requests();
+
+  if ( buffer.size() != 32 ) {
     return {};
   }
 
-  sockaddr_storage address;
-  socklen_t address_length = sizeof( address );
-
-  sockaddr_storage alternate_server;
-  socklen_t alternate_server_length = sizeof( address );
-
-  if ( STUN_USAGE_BIND_RETURN_SUCCESS
-       != stun_usage_bind_process( &message,
-                                   reinterpret_cast<sockaddr*>( &address ),
-                                   &address_length,
-                                   reinterpret_cast<sockaddr*>( &alternate_server ),
-                                   &alternate_server_length ) ) {
+  if ( buffer.at( 0 ) != 1 or buffer.at( 1 ) != 1 ) { /* binding response */
     return {};
   }
 
-  /* don't allow IPv6 for now */
-  if ( address_length != sizeof( sockaddr_in ) or address.ss_family != AF_INET ) {
+  if ( buffer.at( 2 ) != 0 or buffer.at( 3 ) != 12 ) { /* message length = 12 bytes */
     return {};
   }
 
-  return Address { reinterpret_cast<sockaddr*>( &address ), address_length };
+  const string_view transaction_id = buffer.substr( 4, 16 );
+  const auto it = pending_requests_.find( transaction_id );
+  if ( it == pending_requests_.end() ) {
+    return {};
+  }
+
+  pending_requests_.erase( it );
+
+  if ( buffer.at( 20 ) != 0 or buffer.at( 21 ) != 1 ) { /* MAPPED-ADDRESS */
+    return {};
+  }
+
+  if ( buffer.at( 22 ) != 0 or buffer.at( 23 ) != 8 ) { /* attribute length */
+    return {};
+  }
+
+  if ( buffer.at( 24 ) != 0 or buffer.at( 25 ) != 1 ) { /* IPv4 */
+    return {};
+  }
+
+  sockaddr_in address;
+  address.sin_family = AF_INET;
+  memcpy( &address.sin_port, buffer.data() + 26, 2 );
+  memcpy( &address.sin_addr, buffer.data() + 28, 4 );
+
+  return Address { reinterpret_cast<const sockaddr*>( &address ), sizeof( address ) };
 }
--- src/util/stun.hh
+++ src/util/stun.hh
@@ -1,26 +1,28 @@
 #pragma once
 
+#include <map>
 #include <optional>
 #include <string>
-#include <string_view>
-#include <vector>
-
-extern "C"
-{
-#include <stun/stunagent.h>
-}
 
 #include "address.hh"
+#include "random.hh"
 #include "socket.hh"
 
 class STUNClient
 {
-  StunAgent agent_;
-  std::string scratch_ { 64, 'x' };
+  std::mt19937 rng_ = get_random_generator();
+  std::uniform_int_distribution<> dist_ { 0, 255 };
+
+  struct compare_strings : public std::less<std::string_view>
+  {
+    using is_transparent = void;
+  };
+
+  std::map<std::string, uint64_t, compare_strings> pending_requests_ {};
+
+  void expire_pending_requests();
 
 public:
-  STUNClient();
-
-  std::string_view make_binding_request();
+  std::string make_binding_request();
   std::optional<Address> process_binding_response( const std::string_view buffer );
 };
