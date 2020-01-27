9bd99f410fa8d35162e6c7b39c50dd397e79d34f
Sadjad Fouladi <sfouladi@gmail.com>
2020-01-27 11:08:40 -0800

    mycached.cc: Read requests and responses.

--- src/frontend/mycached.cc
+++ src/frontend/mycached.cc
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <unordered_map>
 
+#include "http/http_server.hh"
 #include "util/eventloop.hh"
 #include "util/exception.hh"
 #include "util/socket.hh"
@@ -12,6 +13,18 @@ void usage( char* argv0 )
 {
   cerr << "Usage: " << argv0 << " PORT" << endl;
 }
+
+struct Client
+{
+  uint64_t id;
+  TCPSession session;
+  HTTPServer http {};
+
+  Client( const uint64_t id, TCPSocket&& socket )
+    : id( id )
+    , session( move( socket ) )
+  {}
+};
 
 int main( int argc, char* argv[] )
 {
@@ -25,6 +38,9 @@ int main( int argc, char* argv[] )
       return EXIT_FAILURE;
     }
 
+    uint64_t client_id { 0 };
+    unordered_map<uint64_t, Client> clients;
+
     EventLoop event_loop;
 
     const uint16_t port = static_cast<uint16_t>( stoi( argv[1] ) );
@@ -35,16 +51,67 @@ int main( int argc, char* argv[] )
     listen_sock.bind( listen_address );
     listen_sock.listen();
 
-    event_loop.add_rule( "TCP Listener",
-                         listen_sock,
-                         Direction::In,
-                         [&listen_sock]() {
-                           TCPSocket sock = listen_sock.accept();
-                           cerr << "incoming connection: "
-                                << sock.peer_address().to_string() << endl;
-                         },
-                         []() { return true; },
-                         []() { throw runtime_error( "error" ); } );
+    event_loop.add_rule(
+      "tcp listener",
+      listen_sock,
+      Direction::In,
+      [&listen_sock, &clients, &event_loop, &client_id]() {
+        clients.emplace(
+          piecewise_construct,
+          forward_as_tuple( client_id ),
+          forward_as_tuple( client_id, move( listen_sock.accept() ) ) );
+
+        Client& client = clients.at( client_id );
+
+        event_loop.add_rule(
+          "client read",
+          client.session.socket(),
+          Direction::In,
+          [&client]() { client.session.do_read(); },
+          [&client]() { return client.session.want_read(); } );
+
+        event_loop.add_rule(
+          "client write",
+          client.session.socket(),
+          Direction::Out,
+          [&client]() { client.session.do_write(); },
+          [&client]() { return client.session.want_write(); } );
+
+        event_loop.add_rule(
+          "HTTP read",
+          [&client] { client.http.read( client.session.inbound_plaintext() ); },
+          [&client] {
+            return not client.session.inbound_plaintext()
+                         .readable_region()
+                         .empty();
+          } );
+
+        event_loop.add_rule( "HTTP write",
+                             [&client] {
+                               client.http.write(
+                                 client.session.outbound_plaintext() );
+                             },
+                             [&client]() {
+                               return not client.session.outbound_plaintext()
+                                            .writable_region()
+                                            .empty()
+                                      and not client.http.responses_empty();
+                             } );
+
+        event_loop.add_rule(
+          "got request",
+          [&client]() {
+            cerr << "request received: "
+                 << client.http.requests_front().first_line() << endl;
+
+            client.http.pop_request();
+          },
+          [&client]() { return not client.http.requests_empty(); } );
+
+        client_id++;
+      },
+      []() { return true; },
+      []() { throw runtime_error( "listen socket cancelled" ); } );
 
     while ( event_loop.wait_next_event( -1 ) != EventLoop::Result::Exit )
       ;
